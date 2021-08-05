b9498ee560bf597f0d71fe717c82b91ec788a570
hbrashid <hbrashid@gmail.com>
2021-08-04 19:13:11 -0600

    image change

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
     siteTitleAlt: `Hassan Rashid - Portfolio`,
     siteDescription: `Hassan Rashid - Portfolio - Software Engineer`,
-    siteImage: `public/favicon.png`,
+    siteImage: `/favicon.png`,
   },
   plugins: [
     {
