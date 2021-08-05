23ccca2cfbd2bc14a10838e56596cfd535f480f0
hbrashid <hbrashid@gmail.com>
2021-08-04 19:38:09 -0600

    link tweak

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,8 @@ module.exports = {
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
     siteTitleAlt: `Hassan Rashid - Portfolio`,
     siteDescription: `Hassan Rashid - Portfolio - Software Engineer`,
-    siteUrl: `https://hassanrashid.netlify.app/`,
-    siteImage: `/public/favicon.png`,
+    siteUrl: `https://hassanrashid.netlify.app`,
+    siteImage: `/favicon.png`,
   },
   plugins: [
     {
